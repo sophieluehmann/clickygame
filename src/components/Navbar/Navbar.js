43080c513816a2cbb41e6a2f81f0b876c2905d67
Sophie OT <SophieOT@Sophies-MacBook-Pro.local>
2019-08-03 15:55:34 -0700

    added sticky navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ class Navbar extends React.Component{
     render() {
         return (
             
-            <nav class="navbar navbar-light bg-light">
+            <nav class="navbar navbar-light bg-light" id="sticky">
                 <span class="d-flex justify-content-start">Clicky Game</span>
                 <span class="d-flex justify-content-center">Click an image to start!</span>
                 <span class="d-flex justify-content-end">Current Score {this.props.score} | High Score {this.props.highscore}</span>
