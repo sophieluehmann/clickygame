db2af534da1be84973eb357110d09a780996a370
Sophie OT <SophieOT@D-10-19-124-178.dhcp4.washington.edu>
2019-08-03 13:16:32 -0700

    score count working

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css";
+
+class Navbar extends React.Component{
+    render() {
+        return (
+            
+            <nav class="navbar navbar-light bg-light">
+                <span class="d-flex justify-content-start">Clicky Game</span>
+                <span class="d-flex justify-content-center">Click an image to start!</span>
+                <span class="d-flex justify-content-end">Current Score {this.props.score} | High Score {this.props.highscore}</span>
+            </nav>
+            
+        );
+    }
+}
+
+
+export default Navbar;
